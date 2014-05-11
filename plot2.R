d0cbe56c3ddd1f7f54d160518328d2903f993233
Tom France <chip.france@gmail.com>
2014-05-10 22:25:44 -0400

    initial commit

--- plot2.R
+++ plot2.R
@@ -5,6 +5,6 @@ if (!file.exists("./data/data.Rda")) {
 
 load("./data/data.Rda")
 
-png("./figure/plot2.png")
+png("./figure/plot2.png", width = 480, height = 480)
 with(data, plot(DateTime, Global_active_power, type="l", xlab="", ylab="Global Active Power (kilowatts)"))
 dev.off()
---
+++ plot3.R
@@ -0,0 +1,14 @@
+if (!file.exists("./data/data.Rda")) {
+  source("loadEnvironment.R")
+  loadEnvironment()
+}
+
+load("./data/data.Rda")
+
+png("./figure/plot3.png", width = 480, height = 480)
+with(data, plot(DateTime, Sub_metering_1, type="l", ylab="Energy sub metering", xlab=""))
+with(data, points(DateTime, Sub_metering_2, type="l", col="red"))
+with(data, points(DateTime, Sub_metering_3, type="l", col="blue"))
+legend("topright", cex = 0.95, lty=1, col=c("black", "red", "blue"), legend=c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
+dev.off()
+
